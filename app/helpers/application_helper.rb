6e86dfb2dd98ea37400570180fde9e1da7e59ee4
Gareth Rees <gareth.h.rees@gmail.com>
2013-04-07 23:44:35 +0100

    YOLO!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def gravatar_image_url(email, gravatar_overrides = {})
+    email = email.strip.downcase if email.is_a? String
+    GravatarImageTag::gravatar_url(email, gravatar_overrides)
+  end
+
 end
